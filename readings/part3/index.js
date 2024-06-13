2a12ab64d245ca9ae575bb6db784da32f8d6b9d4
Ethan Coon <eethanccoon@gmail.com>
2024-06-13 15:26:49 -0400

    Built reading's notes app

--- readings/part3/index.js
+++ readings/part3/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
+app.use(express.static('dist'))
+app.use(cors())
 
 let notes = [
     {
@@ -73,7 +77,7 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
