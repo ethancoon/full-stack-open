ac71dcc1bffb162b58ec455874d38216a9b2f6ec
Ethan Coon <eethanccoon@gmail.com>
2024-06-14 16:10:17 -0400

    Completed 3.12

---
+++ readings/part3/mongo.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://ethancoon:${password}@fso-part3-db.bx37oq8.mongodb.net/noteApp?retryWrites=true&w=majority&appName=FSO-Part3-DB`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is easy',
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
