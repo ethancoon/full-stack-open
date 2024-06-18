02d3dcb48b776b78385eb1c264926877d66812e6
Ethan Coon <eethanccoon@gmail.com>
2024-06-18 18:25:25 -0400

    Completed 4.17

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -3,8 +3,8 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({})
-    response.json(users)
+  const users = await User.find({}).populate('blogs', { url: 1, title: 1, author: 1, id: 1 })
+  response.json(users)
 })
 
 usersRouter.post('/', async (request, response) => {
